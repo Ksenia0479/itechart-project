8f563bcd21436d35dc845b636d06299397bb357c
Ksenia <kseniyachervonaya@gmail.com>
2019-02-14 20:59:36 +0300

    test2

---
+++ build/precache-manifest.50b767bcc0e8d61d99b3211ffc26cb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba58b6d30be33d88677b",
+    "url": "/static/css/main.8e21b212.chunk.css"
+  },
+  {
+    "revision": "ba58b6d30be33d88677b",
+    "url": "/static/js/main.ba58b6d3.chunk.js"
+  },
+  {
+    "revision": "06952d6c41f5cf34cdeb",
+    "url": "/static/js/1.06952d6c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b61f0452a1219c83c2635e33515d315e",
+    "url": "/index.html"
+  }
+];
